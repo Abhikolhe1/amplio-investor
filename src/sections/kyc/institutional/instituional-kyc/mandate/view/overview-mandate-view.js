3f795d38299c8f9eea7e1ec7a2b10d69626d704a
Pratik813335 <girasepratiksing@gmail.com>
2026-04-08 16:25:42 +0530

    bug fixed

--- src/sections/kyc/institutional/instituional-kyc/mandate/view/overview-mandate-view.js
+++ src/sections/kyc/institutional/instituional-kyc/mandate/view/overview-mandate-view.js
@@ -40,6 +40,7 @@ export default function OverviewMandateView({
   const { enqueueSnackbar } = useSnackbar();
   const [isSaving, setIsSaving] = useState(false);
   const { investmentMandates, loading: mandateLoading } = useGetInvestmentMandates();
+  const [isInitialized, setIsInitialized] = useState(false);
 
   const existingMandate = useMemo(
     () => normalizeInvestmentMandate(investmentMandates),
@@ -172,25 +173,28 @@ export default function OverviewMandateView({
     percent(calculatePercent());
   }, [calculatePercent, percent]);
 
-  useEffect(() => {
-    if (existingMandate && !mandateLoading) {
-      reset(defaultValues);
 
-      if (!dataInitializedSteps?.includes('kyc_investment_mandate')) {
-        setDataInitializedSteps?.();
-        setActiveStepId?.();
-      }
+
+useEffect(() => {
+  if (!isInitialized && existingMandate && !mandateLoading) {
+    reset(defaultValues);
+    setIsInitialized(true);
+
+    if (!dataInitializedSteps?.includes('kyc_investment_mandate')) {
+      setDataInitializedSteps?.();
+      setActiveStepId?.();
     }
-  }, [
-    dataInitializedSteps,
-    defaultValues,
-    existingMandate,
-    mandateLoading,
-    reset,
-    setActiveStepId,
-    setDataInitializedSteps,
-  ]);
-
+  }
+}, [
+  existingMandate,
+  mandateLoading,
+  reset,
+  defaultValues,
+  dataInitializedSteps,
+  setActiveStepId,
+  setDataInitializedSteps,
+  isInitialized,
+]);
   const onSubmit = handleSubmit(async (data) => {
     try {
       const usersId = sessionStorage.getItem('investor_user_id');
